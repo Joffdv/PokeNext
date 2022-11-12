0c5ad90d78b0e8c7cd659736c8e574290cd6a503
João Emanuel <107960713+Joffdv@users.noreply.github.com>
2022-11-12 19:54:27 +0000

    Projeto Finalizado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,48 @@
-import styles from '../styles/Home.module.css'
+import Card from "../components/Card";
 
-export default function Home() {
-  return <h1>Poke Next</h1>
+import Image from "next/image";
+
+import styles from "../styles/Home.module.css";
+
+export async function getStaticProps() {
+  const maxPokemons = 251;
+  const api = `https://pokeapi.co/api/v2/pokemon/`;
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+
+  const data = await res.json();
+
+  // add pokemon index
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
+}
+
+export default function Home({ pokemons }) {
+  return (
+    <>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>
+          Poke<span>Next</span>
+        </h1>
+        <Image
+          src="/images/pokeball.png"
+          width="50"
+          height="50"
+          alt="PokeNext"
+        />
+      </div>
+      <div className={styles.pokemon_container}>
+        {pokemons.map((pokemon) => (
+          <Card key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </div>
+    </>
+  );
 }
